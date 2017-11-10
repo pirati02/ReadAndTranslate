97b51f9b2da45ed162fced94d38e6bdf524eb7d5
pirati02 <me280820>
2017-11-11 01:23:41 +0400

    added stupid network checking

--- readandtranslate/src/main/java/ge/dev/baqari/readandtranslate/service/ClipboardService.kt
+++ readandtranslate/src/main/java/ge/dev/baqari/readandtranslate/service/ClipboardService.kt
@@ -5,20 +5,24 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.graphics.PixelFormat
+import android.net.ConnectivityManager
 import android.os.IBinder
 import android.util.DisplayMetrics
 import android.view.Gravity
 import android.view.WindowManager
+import android.widget.Toast
 import ge.dev.baqari.readandtranslate.App
 import ge.dev.baqari.readandtranslate.serviceHelper.ServiceCommangs.Companion.START_CLIPBOARD_MANAGER
 import ge.dev.baqari.readandtranslate.serviceHelper.ServiceCommangs.Companion.STOP_CLIPBOARD_MANAGER
 import ge.dev.baqari.readandtranslate.ui.TranslateOverlay
+import ge.dev.baqari.si.R
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class ClipboardService : Service(), ClipboardManager.OnPrimaryClipChangedListener {
 
     private var clipBoardManager: ClipboardManager? = null
+    private var connectivityManager: ConnectivityManager? = null
     private var translateOverlay: TranslateOverlay? = null
     private var windowManager: WindowManager? = null
 
@@ -26,9 +30,10 @@ class ClipboardService : Service(), ClipboardManager.OnPrimaryClipChangedListene
         super.onCreate()
         if (clipBoardManager == null)
             clipBoardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
-
         if (windowManager == null)
             windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        if (connectivityManager == null)
+            connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
         val displayMetrics = DisplayMetrics()
         windowManager!!.defaultDisplay.getMetrics(displayMetrics)
@@ -71,20 +76,23 @@ class ClipboardService : Service(), ClipboardManager.OnPrimaryClipChangedListene
     }
 
     private fun translate(text: String) {
-        (applicationContext as App).
-                apiClient?.
-                transalteApi?.
-                translate(text)?.
-                observeOn(AndroidSchedulers.mainThread())?.
-                subscribeOn(Schedulers.io())?.
-                subscribe({
-                    val result = it.rows?.map { i ->
-                        i.value?.text
-                    }
-                    translateOverlay?.show(text, result)
-                }, {
-                    it.printStackTrace()
-                    translateOverlay?.hide()
-                })
+        if (connectivityManager?.activeNetworkInfo?.isConnected!!)
+            (applicationContext as App).
+                    apiClient?.
+                    transalteApi?.
+                    translate(text)?.
+                    observeOn(AndroidSchedulers.mainThread())?.
+                    subscribeOn(Schedulers.io())?.
+                    subscribe({
+                        val result = it.rows?.map { i ->
+                            i.value?.text
+                        }
+                        translateOverlay?.show(text, result)
+                    }, {
+                        it.printStackTrace()
+                        translateOverlay?.hide()
+                    })
+        else
+            Toast.makeText(this, getString(R.string.no_network), Toast.LENGTH_SHORT).show()
     }
 }
